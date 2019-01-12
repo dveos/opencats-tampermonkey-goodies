fd30084ee779fdb5f50ca10a8eeb42467330a01b
dveos <Nathan@elcproperty.co.uk>
2019-01-12 22:46:12 +0000

    Update
    - Update to LinkedIn elements

--- linkedin-add-to-cats.user.js
+++ linkedin-add-to-cats.user.js
@@ -1,14 +1,14 @@
 // ==UserScript==
 // @name         LinkedIn-Add-To-OpenCats
 // @namespace    LinkedIn
-// @version      0.1
+// @version      0.2
 // @description  try to take over the world!
 // @author       You
 // @match        https://www.linkedin.com/*
 // @grant        none
 // @updateURL    https://raw.githubusercontent.com/mlespiau/opencats-tampermonkey-goodies/master/linkedin-add-to-cats.user.js
 // @downloadURL  https://raw.githubusercontent.com/mlespiau/opencats-tampermonkey-goodies/master/linkedin-add-to-cats.user.js
-// @require  https://gist.github.com/raw/2625891/waitForKeyElements.js
+// @require      https://gist.github.com/raw/2625891/waitForKeyElements.js
 // @noframes
 // ==/UserScript==
 
@@ -22,11 +22,11 @@
         }
     }
     ready(document, function() {
-        var nameNode = document.querySelectorAll('.full-name');
+        var nameNode = document.querySelectorAll('.pv-top-card-section__name');
         if (nameNode) {
             var fullName = nameNode[0].textContent;
             var company = '';
-            var companyNode = document.querySelectorAll('#overview-summary-current td a');
+            var companyNode = document.querySelectorAll('.pv-top-card-v2-section__company-name');
             if (companyNode) {
                 company = companyNode[0].textContent;
             }
@@ -49,11 +49,11 @@
             link.appendChild(img);
             nameNode[0].insertBefore(link, nameNode[0].firstChild);
             link.addEventListener('click', function() {
-                var popup =  window.open(searchCandidate, '_blank', 'width=1200,height=600');
+                var popup = window.open(searchCandidate, '_blank', 'width=1200,height=600');
             });
         }
-        waitForKeyElements ("#email-view", function() {
-            var emailNode = document.querySelectorAll('#email-view');
+        waitForKeyElements (".ci-email", function() {
+            var emailNode = document.querySelectorAll('.ci-email a');
             if (emailNode) {
                 var emailAddress = emailNode[0].textContent;
                 var searchCandidateByEmail = 'http://ec2-174-129-137-61.compute-1.amazonaws.com/index.php?m=candidates&parameterscandidates%3AcandidatesListByViewDataGrid=a%3A6%3A%7Bs%3A10%3A%22rangeStart%22%3Bi%3A0%3Bs%3A10%3A%22maxResults%22%3Bi%3A15%3Bs%3A13%3A%22filterVisible%22%3Bb%3A1%3Bs%3A6%3A%22sortBy%22%3Bs%3A15%3A%22currentEmployer%22%3Bs%3A13%3A%22sortDirection%22%3Bs%3A4%3A%22DESC%22%3Bs%3A6%3A%22filter%22%3Bs%3A9%3A%22%3Cdynamic%3E%22%3B%7D&dynamicArgumentcandidates%3AcandidatesListByViewDataGrid=%3B%2CE-Mail%3D%3D' + encodeURIComponent(emailAddress) + '%2C';
